593b2a4befa22c40e5c6a71cb3fe73a861033b62
Serafim <xvarlordx@gmail.com>
2021-10-28 13:49:07 +0300

    homework-8 done! Долго ломал голову, что засунуть в конструктор Game, в чате сказали, что его можно оставить пустым)))

--- main.js
+++ main.js
@@ -1,60 +1,6 @@
 'use strict'
-import {player1, player2} from "./players.js";
-import {createElement, $arenas, $formFight} from "./utils.js";
-import {generateLogs,enemyAttack, playerAttack, showResult} from "./fight.js";
+import {Game} from './Game.js'
 
+const game = new Game();
 
-//создание игрока
-const createPlayer = (playerobj) => {
-
-    const $player = createElement('div', 'player'+playerobj.player);
-    const $progressbar = createElement('div', 'progressbar')
-    const $life = createElement('div','life' )
-    const $name = createElement('div', 'name')
-    const $character = createElement('div', 'character')
-    const $img = createElement('img')
-
-    $life.style.width = playerobj.hp + "%"
-    $name.innerText = playerobj.name
-    $img.src= playerobj.img
-
-    $character.appendChild($img)
-    $player.appendChild($progressbar)
-    $player.appendChild($character)
-    $progressbar.appendChild($life)
-    $progressbar.appendChild($name)
-    return $player
-}
-
-//рендер игроков
-$arenas.appendChild(createPlayer(player1));
-$arenas.appendChild(createPlayer(player2));
-
-//показ сообщения о старте файте
-generateLogs('start', player1, player2);
-
-//кнопка боя
-$formFight.addEventListener('submit', function (e) {
-    e.preventDefault();
-    enemyAttack();
-    const enemy = enemyAttack();
-    const player = playerAttack();
-
-    if (enemy.hit === player.defence) {
-        player1.changeHP(enemy.value);
-        player1.renderHP(player1);
-        generateLogs('hit', player2, player1, enemy.value, player1.hp);
-    } else {
-        generateLogs('defence', player2, player1);
-    }
-
-    if (player.hit === enemy.defence) {
-        player2.changeHP(player.value);
-        player2.renderHP(player2);
-        generateLogs('hit', player1, player2, player.value, player2.hp);
-    } else {
-        generateLogs('defence', player1, player2);
-    }
-
-    showResult();
-});
+game.start();
--- players.js
+++ players.js
@@ -1,42 +1,59 @@
-export const player1 = {
+import {createElement} from "./utils.js";
+
+class Player {
+    constructor(props) {
+        this.player = props.player;
+        this.name =  props.name;
+        this.hp = props.hp;
+        this.img = props.img;
+    }
+
+     changeHP =  (numberHP) => {
+        this.hp -= numberHP;
+
+        if (this.hp <= 0) {
+            this.hp = 0;
+        }
+    }
+
+     elHP = () => document.querySelector(`.player${this.player} .life`);
+
+     renderHP = () => {this.elHP().style.width = this.hp + '%';}
+}
+
+export const player1 = new Player( {
     player: 1,
     name: "Scorpion",
     hp: 100,
     img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
-    weapon: ["minigun", "knife"],
-    attack: function (name) {
-        console.log(name + "Fight...")
-    },
-    elHP,
-    changeHP,
-    renderHP,
-}
-export const player2 = {
+})
+export const player2 =  new Player( {
     player: 2,
     name: "Kitana",
     hp: 100,
     img: "http://reactmarathon-api.herokuapp.com/assets/kitana.gif",
-    weapon: ["plasmagun", "fist"],
-    attack: function (name) {
-        console.log(name + "Fight...")
-    },
-    elHP,
-    changeHP,
-    renderHP,
+})
+
+//Создание игрока
+export const createPlayer = ({player, hp, name, img}) => {
+
+    const $player = createElement('div', `player${player}`);
+    const $progressbar = createElement('div', 'progressbar')
+    const $life = createElement('div','life' )
+    const $name = createElement('div', 'name')
+    const $character = createElement('div', 'character')
+    const $img = createElement('img')
+
+    $life.style.width = hp + "%"
+    $name.innerText = name
+    $img.src= img
+
+    $character.appendChild($img)
+    $player.appendChild($progressbar)
+    $player.appendChild($character)
+    $progressbar.appendChild($life)
+    $progressbar.appendChild($name)
+    return $player
 }
 
-function changeHP (numberHP) {
-    this.hp -= numberHP;
 
-    if (this.hp <= 0) {
-        this.hp = 0;
-    }
-}
-
-function elHP () {
-    return document.querySelector('.player'+ this.player +' .life');
-}
-
-function renderHP (player) {
-    player.elHP().style.width = this.hp + '%';
-}
